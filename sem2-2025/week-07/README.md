38fe296794f01add04f03533c262c44ccc0b4a07
Clement Chau <142649232+Realify2005@users.noreply.github.com>
2025-09-08 12:46:39 +1000

    add week 7 2025s2 topics

---
+++ sem2-2025/week-07/README.md
@@ -0,0 +1,3 @@
+# Week 7 Topics
+* Code structure, Variable names, Test cases
+* Docstring and commenting
---
+++ sem2-2025/week-07/exercise-3.py
@@ -0,0 +1,4 @@
+def remove_negative(nums):
+    for num in nums:
+        if num < 0:
+            nums.remove(num)
